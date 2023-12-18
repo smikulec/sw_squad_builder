ecce351341a2f5d9a23218a97e2d24d1e5e25ebe
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-12-18 22:09:45 +0100

    Refactor to redirect to dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,19 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const config = { matcher: ['/dashboard'] };
+export default withAuth(
+  function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname === '/') {
+      {
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+      }
+    }
+  },
+  {
+    pages: {
+      signIn: '/login',
+    },
+  },
+);
+
+export const config = { matcher: ['/dashboard', '/'] };
