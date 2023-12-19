1a3899532eb542ae4832948f678b76e693ce02ac
Sonja Mikulec <sonja.mikulec@gmail.com>
2023-12-19 15:53:17 +0100

    Remove unnecessary button and optimized routes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,27 +2,20 @@
 import { Button } from '../ui';
 import { signOut } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
+import { ROUTES } from '@/routes';
 
 const Header = () => {
   const pathname = usePathname();
 
-  const handleCreateSquad = () => {
-    // Logic for creating a squad
-  };
-
-  if (pathname.includes('/login') || pathname.includes('signin')) {
+  if (
+    pathname.includes(ROUTES.LOGIN_URL) ||
+    pathname.includes(ROUTES.SIGNIN_URL)
+  ) {
     return null;
   }
 
   return (
     <header className="header flex justify-end gap-8 bg-black/70 py-8 pr-12">
-      <Button
-        variant="outline"
-        onClick={handleCreateSquad}
-        className="text-white"
-      >
-        Create Squad
-      </Button>
       <Button variant="ghost" onClick={() => signOut()} className="text-white">
         Logout
       </Button>
